6539b4104e1e311b63dc5717482830ee2db960c8
RenShuhuai-Andy <739584819@qq.com>
2019-09-19 22:54:16 +0800

    HW01-alpha

---
+++ HW01-sphere/src/ball.cpp
@@ -0,0 +1,192 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <vector>
+#include "shader/shader.h"
+#include <direct.h>
+#include <string>
+#include <iostream>
+using namespace std;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+const unsigned int Y_SEGMENTS = 100;
+const unsigned int X_SEGMENTS = 100;
+const float PI = 3.1415926;
+
+template<typename T> T* vec2arr(vector<T>& vec) {
+	T* arr = new T[vec.size()];
+	if (!vec.empty()) {
+		memcpy(arr, &vec[0], vec.size() * sizeof(T));
+	}
+	return arr;
+}
+
+int main()
+{
+	// glfw: initialize and configure
+	// ------------------------------
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // uncomment this statement to fix compilation on OS X
+#endif
+
+	// glfw window creation
+	// --------------------
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	// glad: load all OpenGL function pointers
+	// ---------------------------------------
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	// build and compile our shader program
+	// ------------------------------------
+	Shader ourShader("res/shader/3.3.shader.vs", "res/shader/3.3.shader.fs"); // you can name your shader files however you like
+
+	// set up vertex data (and buffer(s)) and configure vertex attributes
+	// ------------------------------------------------------------------
+	//float vertices[] = {
+	//	// positions         // colors
+	//	 0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right
+	//	-0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
+	//	 0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // top 
+	//};
+
+
+	// 生成球的顶点 
+	vector<float> sphereVertices;
+	//float sphereVertices[100] = {};
+	for (int y = 0; y <= Y_SEGMENTS; y++) {
+		for (int x = 0; x <= X_SEGMENTS; x++) {
+			float xSegment = (float)x / (float)X_SEGMENTS;
+			float ySegment = (float)y / (float)Y_SEGMENTS;
+			float xPos = cos(xSegment * 2.0f * PI) * sin(ySegment * PI);
+			float yPos = cos(ySegment * PI);
+			float zPos = sin(xSegment * 2.0f * PI) * sin(ySegment * PI);
+			sphereVertices.push_back(xPos);
+			sphereVertices.push_back(yPos);
+			sphereVertices.push_back(zPos);
+		}
+	}
+	float* vertices = vec2arr(sphereVertices);
+
+
+	//根据球面上每一点的坐标，去构造一个一个三角形顶点数组 
+	vector<int> sphereIndices;
+	for (int i = 0; i < Y_SEGMENTS; i++) { 
+		for (int j = 0; j < X_SEGMENTS; j++) { 
+			sphereIndices.push_back(i * (X_SEGMENTS+1) + j); 
+			sphereIndices.push_back((i + 1) * (X_SEGMENTS + 1) + j); 
+			sphereIndices.push_back((i + 1) * (X_SEGMENTS + 1) + j + 1); 
+			sphereIndices.push_back(i * (X_SEGMENTS + 1) + j); 
+			sphereIndices.push_back((i + 1) * (X_SEGMENTS + 1) + j + 1); 
+			sphereIndices.push_back(i * (X_SEGMENTS + 1) + j + 1); 
+		} 
+	}
+	int* indices = vec2arr(sphereIndices);
+
+
+	unsigned int VBO, VAO, EBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sphereVertices.size() * sizeof(float), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sphereIndices.size() * sizeof(int), indices, GL_STATIC_DRAW);
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	// color attribute
+	//glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	//glEnableVertexAttribArray(1);
+
+	// note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	// You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+	// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+	glBindVertexArray(0);
+
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);//使用线框模式绘制
+	//开启面剔除(只需要展示一个面，否则会有重合) 
+	glEnable(GL_CULL_FACE); 
+	glCullFace(GL_BACK);
+	// render loop
+	// -----------
+	while (!glfwWindowShouldClose(window))
+	{
+		// input
+		// -----
+		processInput(window);
+
+		// render
+		// ------
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// render the triangle
+		ourShader.use();
+		glBindVertexArray(VAO);
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, sphereIndices.size(), GL_UNSIGNED_INT, 0);
+
+		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+		// -------------------------------------------------------------------------------
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	// optional: de-allocate all resources once they've outlived their purpose:
+	// ------------------------------------------------------------------------
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
+
+	// glfw: terminate, clearing all previously allocated GLFW resources.
+	// ------------------------------------------------------------------
+	glfwTerminate();
+	return 0;
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	// make sure the viewport matches the new window dimensions; note that width and 
+	// height will be significantly larger than specified on retina displays.
+	glViewport(0, 0, width, height);
+}
+
+
---
+++ README.md
@@ -0,0 +1,19 @@
+## 计算机图形学课后作业
+ ![](https://img.shields.io/badge/release-v1.0-yellowgreen.svg) ![](https://img.shields.io/badge/platform-windows-brightgreen.svg) ![](https://img.shields.io/badge/build-cmake-blue.svg)
+
+### 使用方法
+
+请用 CMake 工具打开该工程，具体方法是：
+
+1. 下载 CMake：[地址](http://www.cmake.org/cmake/resources/software.html)。我选择的是 Win32 安装程序。
+
+2. 在 CMake 的源码路径处填入文件夹的路径，如作业一是 `xxx/HW01-sphere`。
+3. 在 CMake 的编译输出路径处填入build的路径，如作业一是 `xxx/HW01-sphere/build`。
+4. configure 两次再 generate（注意选择适合自己的 IDE 版本）。
+5. 打开 build 中编译好的文件即可。（如，选择 Visual Studio 则打开 *.sln）。
+6. 右键工程（如作业一是 `HW01-sphere`），点击 "设为启动项目"，然后运行。
+7. note: 编译目录不要有特殊字符。配置有问题参考：[地址]([https://learnopengl-cn.github.io/01%20Getting%20started/02%20Creating%20a%20window/#glfw_1](https://learnopengl-cn.github.io/01 Getting started/02 Creating a window/#glfw_1))。
+
+### 实验作业
+
+* 作业一：绘制球体 - `HW01-sphere` 。
